af6dd73ee505ce2fcb165a7f96e39f536d877b1e
Michelyserras <Michelysrrs@gmail.com>
2024-03-03 12:03:51 -0300

    exclusao de arquivo sem uso

--- Programa-o-para-Web-main/Aula_3_Funcoes/Aula_3_Funcoes/src/ex2.ts
+++
@@ -1,8 +0,0 @@
-// typeof: verifica o tipo de uma variavel primitiva (number, string, boolean)
-// instace of array : verifica o tipo de uma variavel não primitiva (array,)
-// array é não primitiva porque é um conjunto de variaveeis não primitivas 
-
-/* 
-any: aceita qualquer tipo, mas é possível restringir a função criando uma chamada e tipando ela;
-
-*/
