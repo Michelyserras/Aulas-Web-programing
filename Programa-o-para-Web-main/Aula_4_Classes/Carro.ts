4162716af27077842b009b0cccec3b9d2e3a5210
Michelyserras <Michelysrrs@gmail.com>
2024-03-06 22:14:13 -0300

    Exercicio1 - carros completar get/set/metodoSUm

--- Programa-o-para-Web-main/Aula_4_Classes/Carro.ts
+++ Programa-o-para-Web-main/Aula_4_Classes/Carro.ts
@@ -1,8 +1,8 @@
 class Carro {
-    marca: string;
-    modelo: string;
-    ano: number;
-    cor: string;
+    private marca: string;
+    private modelo: string;
+    private ano: number;
+    private cor: string;
   
     constructor(marca: string, modelo: string, ano: number, cor: string) {
       this.marca = marca;
@@ -11,11 +11,56 @@ class Carro {
       this.cor = cor;
     }
   
+    get getmarca(): string{
+      return this.getmarca;
+
+    }
+
+    set setmarca(marca : string){
+        this.marca = marca;
+
+    } 
+
+    get getmodelo(): string{
+      return this.getmodelo;
+
+    }
+
+    set setmodelo(modelo : string){
+        this.modelo = modelo;
+
+    } 
+
+    get getano(): number{
+      return this.getano;
+
+    }
+
+    set setano(ano : number){
+        this.ano = ano;
+
+    } 
+
+    get getcor(): string{
+      return this.getcor;
+
+    }
+
+    set setcor(cor : string){
+        this.cor = cor;
+
+    } 
+    
+    calcularIdade(anoAtual: number): number{
+      return anoAtual - this.ano;
+    };
+ 
     exibirInfo(): void {
       console.log(`Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}, Cor: ${this.cor}`);
     }
   }
   
+   
   const meuCarro = new Carro("Toyota", "Corolla", 2020, "Prata");
   
   meuCarro.exibirInfo();
