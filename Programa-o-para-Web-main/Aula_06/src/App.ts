e8b193bd496b4eb9911bd91040b51cce8bc7496c
Michelyserras <michelybrrs@gmail>
2024-03-20 21:30:23 -0300

    aula_API_20/03

---
+++ Programa-o-para-Web-main/Aula_06/src/App.ts
@@ -0,0 +1,10 @@
+export class Pessoa{
+    private _nome: string;
+    private _anoNascimento: number;
+
+    constructor(_nome:string, _anoNascimento: number){
+        this._nome = _nome;
+        this._anoNascimento = _anoNascimento;
+    }
+    
+}
