539f85154bed409e94571f2a642d12951d56778a
Michelyserras <Michelysrrs@gmail.com>
2024-06-21 19:54:27 -0300

    aula 13 - callback

---
+++ aulas_web/aula_10_funcoesAssincronas/src/callback.ts
@@ -0,0 +1,16 @@
+// callback é a resposta que se espera após executar uma função o
+
+export function imprimir( value: any):void{
+    console.log("A operação efetuada resultou em: " + value);
+}
+
+
+export function concatenar( a: string, b: string, callback: (param:any)=> void):void{
+    var op = a + " " + b;
+    callback(op);
+}
+
+export function somar( a: number, b: number, callback: (param:any)=> void ):void{
+    var op = a + b;
+    callback(op);
+}
