60d3ac7cf5439a1ad51596830907121674f4e521
Michelyserras <Michelysrrs@gmail.com>
2024-07-01 21:24:55 -0300

    adicionando arquivo para itens vendas

---
+++ aulas_web/Aula_09_ProjetoFinal/src/Repository/itensRepository.ts
@@ -0,0 +1,12 @@
+
+import { ItemVenda } from "../Model/itemVenda";
+import { getItens } from "../global/global";
+
+export class itensRepository{
+    itensList: ItemVenda[] = getItens();
+
+    adicionarItens(itens: ItemVenda){
+        return this.itensList.push(itens);
+    }
+    
+}
