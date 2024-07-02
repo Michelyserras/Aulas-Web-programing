f886113eae03a7cb9532dbb23924fb6a112632da
Michelyserras <Michelysrrs@gmail.com>
2024-07-01 22:07:17 -0300

    começando parte de vendas

--- aulas_web/Aula_09_ProjetoFinal/src/Service/VendaService.ts
+++ aulas_web/Aula_09_ProjetoFinal/src/Service/VendaService.ts
@@ -1,50 +1,56 @@
+import { ItemVenda } from "../Model/itemVenda";
 import { VendaPaes } from "../Model/vendaPaes";
 import { EstoqueRepository } from "../Repository/EstoqueRepository";
 import { VendaRepository } from "../Repository/VendaRepository";
+import { itensRepository } from "../Repository/itensRepository";
 import { EstoqueService } from "./EstoqueService";
 
  
 
-export class VendaService{
+export class VendaService {
     vendaRepository: VendaRepository= new VendaRepository();
     estoqueRepository: EstoqueRepository = new EstoqueRepository();
     estoqueService: EstoqueService = new EstoqueService();
 
+    itensRepository: itensRepository = new itensRepository();
+
     // vendas
     cadastrarCompra(compraData: VendaPaes){
         const { cpfCliente, itensComprados} = compraData;
-        let valorTotal = 0;
+    
 
-        if(!cpfCliente || itensComprados.length == 0){
+        if(!cpfCliente || itensComprados){
             throw new Error("Informações incompletas");
         }
-
-      
-    // verificando se estoque existe e somando o mesmo;
-        for (const item of itensComprados){
-            const estoque = this.estoqueService.ProcurarPorID(item.estoqueId);
         
-            if( !estoque || estoque.quantidade < item.quantidade){
-                throw new Error("Produto fora de estoque");
-            }
-            valorTotal += estoque.precoVenda * item.quantidade;
-        
-        }
-
-        const novaCompra = new VendaPaes(cpfCliente, valorTotal, itensComprados);
-        this.vendaRepository.realizarVenda(novaCompra);
-
-        for (const item of itensComprados) {
-            const estoqueItem = this.estoqueService.ProcurarPorID(item.estoqueId);
-            if (estoqueItem) {
-                estoqueItem.quantidade -= item.quantidade;
-                this.estoqueService.atualizarEstoque(estoqueItem);
-            }
-        }
-
-        return novaCompra;
+       
     }
-    
+      
+    // // verificando se estoque existe e somando o mesmo;
+    //     for (const item of itensComprados){
+    //         const estoque = this.estoqueService.ProcurarPorID(item.estoqueId);
+        
+    //         if( !estoque || estoque.quantidade < item.quantidade){
+    //             throw new Error("Produto fora de estoque");
+    //         }
+    //         valorTotal += estoque.precoVenda * item.quantidade;
+        
+    //     }
+
+    //     const novaCompra = new VendaPaes(cpfCliente, valorTotal, itensComprados);
+    //     this.vendaRepository.realizarVenda(novaCompra);
+
+    //     for (const item of itensComprados) {
+    //         const estoqueItem = this.estoqueService.ProcurarPorID(item.estoqueId);
+    //         if (estoqueItem) {
+    //             estoqueItem.quantidade -= item.quantidade;
+    //             this.estoqueService.atualizarEstoque(estoqueItem);
+    //         }
+    //     }
+
+    //     return novaCompra;
+    // 
+   
 }
 
     
