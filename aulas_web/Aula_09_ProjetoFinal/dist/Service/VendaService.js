97658c9514967daf7357c06133dc81438c1dcb21
Michelyserras <Michelysrrs@gmail.com>
2024-07-02 15:34:23 -0300

    documentação da parte vendas

--- aulas_web/Aula_09_ProjetoFinal/dist/Service/VendaService.js
+++ aulas_web/Aula_09_ProjetoFinal/dist/Service/VendaService.js
@@ -10,45 +10,6 @@ class VendaService {
         this.vendaRepository = new VendaRepository_1.VendaRepository();
         this.estoqueRepository = new EstoqueRepository_1.EstoqueRepository();
         this.modalidadeRepository = new ModalidadeRepository_1.ModalidadeRepository();
-        //     this.vendaRepository.vendasList.forEach(itensComprados => {
-        //         this.itensRepository.itensList.forEach(produto => {
-        //             const itens = new ItemVenda(produto.estoqueId, produto.quantidade);
-        //             const estoqueExiste = this.estoqueRepository.consultaEstoqueId(produto.estoqueId); // constante que vai verificar se o estoque existe
-        //             console.log("Estoque existe", estoqueExiste);
-        //             if(estoqueExiste){
-        //                 if(produto.quantidade > estoqueExiste.quantidade){ // verificando se há estoque suficiente para realizar a venda
-        //                     throw new Error("Estoque do produto é insuficiente para realizar a venda!" + estoqueExiste);
-        //                 } 
-        //                 else if(produto.quantidade < estoqueExiste.quantidade){// caso haja, vamos adicionar os produtos a lista de itens;
-        //                      // adicionando produtos na lista
-        //                     console.log("Adicionando itens a compra", itens);
-        //                 }
-        //                 this.itensRepository.adicionarItens(itens);
-        //             } 
-        //         });
-        //     }
-        //     const produtos = this.itensRepository.itensList;
-        //    
-        // }
-        // // verificando se estoque existe e somando o mesmo;
-        //     for (const item of itensComprados){
-        //         const estoque = this.estoqueService.ProcurarPorID(item.estoqueId);
-        //         if( !estoque || estoque.quantidade < item.quantidade){
-        //             throw new Error("Produto fora de estoque");
-        //         }
-        //         valorTotal += estoque.precoVenda * item.quantidade;
-        //     }
-        //     const novaCompra = new VendaPaes(cpfCliente, valorTotal, itensComprados);
-        //     this.vendaRepository.realizarVenda(novaCompra);
-        //     for (const item of itensComprados) {
-        //         const estoqueItem = this.estoqueService.ProcurarPorID(item.estoqueId);
-        //         if (estoqueItem) {
-        //             estoqueItem.quantidade -= item.quantidade;
-        //             this.estoqueService.atualizarEstoque(estoqueItem);
-        //         }
-        //     }
-        //     return novaCompra;
-        // 
     }
     // vendas
     cadastrarCompra(compraData) {
@@ -59,14 +20,15 @@ class VendaService {
         }
         for (let i = 0; i < itensComprados.length; i++) { // loop usado para percorrer a lista de itens 
             const estoque = this.estoqueRepository.consultaEstoqueId(itensComprados[i].estoqueId); //verificando se o estoque existe
-            if (estoque) {
-                if (estoque.quantidade < itensComprados[i].quantidade) {
+            if (estoque) { // a compra apenas sera realizada se houver um estoque disponivel
+                if (estoque.quantidade < itensComprados[i].quantidade) { // se o a quantidade disponivel de produtos for menor que a quantidade comprada
                     throw new Error("Estoque insuficiente...");
                 }
                 else {
-                    itensComprados[i].nome = this.modalidadeRepository.filtrarPorNome(estoque.modalidadeId);
+                    // caso contrário a venda será realizada
+                    itensComprados[i].nome = this.modalidadeRepository.filtrarPorNome(estoque.modalidadeId); // adicionando o nome do produto na venda
                     console.log("estoque atual", estoque);
-                    total += itensComprados[i].quantidade * estoque.precoVenda;
+                    total += itensComprados[i].quantidade * estoque.precoVenda; // somando valor total
                     console.log("somando valores", total);
                     estoque.quantidade -= itensComprados[i].quantidade; //atualizando quantidade de itens no estoque
                     console.log("estoque depois da compra", estoque);
--- aulas_web/Aula_09_ProjetoFinal/src/Service/VendaService.ts
+++ aulas_web/Aula_09_ProjetoFinal/src/Service/VendaService.ts
@@ -26,16 +26,18 @@ export class VendaService {
 
             const estoque = this.estoqueRepository.consultaEstoqueId(itensComprados[i].estoqueId); //verificando se o estoque existe
 
-            if(estoque){
-                if(estoque.quantidade < itensComprados[i].quantidade){
+            if(estoque){ // a compra apenas sera realizada se houver um estoque disponivel
+
+                if(estoque.quantidade < itensComprados[i].quantidade){ // se o a quantidade disponivel de produtos for menor que a quantidade comprada
                     throw new Error("Estoque insuficiente...");
                 }
                 else{
-                   
-                    itensComprados[i].nome = this.modalidadeRepository.filtrarPorNome(estoque.modalidadeId);
+                   // caso contrário a venda será realizada
+
+                    itensComprados[i].nome = this.modalidadeRepository.filtrarPorNome(estoque.modalidadeId); // adicionando o nome do produto na venda
 
                     console.log("estoque atual", estoque);
-                    total += itensComprados[i].quantidade * estoque.precoVenda;
+                    total += itensComprados[i].quantidade * estoque.precoVenda; // somando valor total
                     console.log("somando valores", total);
 
                     
@@ -58,64 +60,4 @@ export class VendaService {
         return novaCompra;
     }
 
-
-    //     this.vendaRepository.vendasList.forEach(itensComprados => {
-    //         this.itensRepository.itensList.forEach(produto => {
-    //             const itens = new ItemVenda(produto.estoqueId, produto.quantidade);
-    //             const estoqueExiste = this.estoqueRepository.consultaEstoqueId(produto.estoqueId); // constante que vai verificar se o estoque existe
-    //             console.log("Estoque existe", estoqueExiste);
-                
-    
-            
-    //             if(estoqueExiste){
-    
-    //                 if(produto.quantidade > estoqueExiste.quantidade){ // verificando se há estoque suficiente para realizar a venda
-    //                     throw new Error("Estoque do produto é insuficiente para realizar a venda!" + estoqueExiste);
-    
-    //                 } 
-    //                 else if(produto.quantidade < estoqueExiste.quantidade){// caso haja, vamos adicionar os produtos a lista de itens;
-    //                      // adicionando produtos na lista
-    //                     console.log("Adicionando itens a compra", itens);
-    
-    //                 }
-    //                 this.itensRepository.adicionarItens(itens);
-    
-    //             } 
-            
-                
-    
-    //         });
-
-    //     }
-       
-    //     const produtos = this.itensRepository.itensList;
-        
-    //    
-       
-    // }
-      
-    // // verificando se estoque existe e somando o mesmo;
-    //     for (const item of itensComprados){
-    //         const estoque = this.estoqueService.ProcurarPorID(item.estoqueId);
-        
-    //         if( !estoque || estoque.quantidade < item.quantidade){
-    //             throw new Error("Produto fora de estoque");
-    //         }
-    //         valorTotal += estoque.precoVenda * item.quantidade;
-        
-    //     }
-
-    //     const novaCompra = new VendaPaes(cpfCliente, valorTotal, itensComprados);
-    //     this.vendaRepository.realizarVenda(novaCompra);
-
-    //     for (const item of itensComprados) {
-    //         const estoqueItem = this.estoqueService.ProcurarPorID(item.estoqueId);
-    //         if (estoqueItem) {
-    //             estoqueItem.quantidade -= item.quantidade;
-    //             this.estoqueService.atualizarEstoque(estoqueItem);
-    //         }
-    //     }
-
-    //     return novaCompra;
-    // 
-    }
+}
