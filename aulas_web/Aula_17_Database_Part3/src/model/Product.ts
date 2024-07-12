3aa3ebc879322638144172fa658fab9fbfab2bb7
Michelyserras <Michelyserras@gmail.com>
2024-07-12 20:26:35 -0300

    alterando classe livro - quase feito

--- aulas_web/Aula_17_Database_Part3/src/model/Product.ts
+++ aulas_web/Aula_17_Database_Part3/src/model/Product.ts
@@ -1,10 +1,17 @@
 export class Product{
     id: number;
-    name: string;
-    price: number;
+    title: string;
+    author: string;
+    publishedDate: string;
+    isbn: string;
+    pages: number;
+    language: string;
+    publisher: string;
 
-    constructor(id?:number, name?:string, price?:number){
+    constructor(id?:number,title?:string, author?:string, publishedDate?: string, isbn?: string, pages?: number, language?:string, publisher?:string){
         this.id = id || 0;
+        this.title = title || '';
+        this.author = author || '';
         this.name = name || '';
         this.price = price || 0;
     }
