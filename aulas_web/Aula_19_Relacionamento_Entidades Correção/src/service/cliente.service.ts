bd4c6146bf7b2b1b6a0fc091b8d57d6c58f3bf5a
Michelyserras <michelysrrs@gmail.com>
2024-07-26 20:33:31 -0300

    a

--- aulas_web/Aula_19_Relacionamento_Entidades Correção/src/service/cliente.service.ts
+++ aulas_web/Aula_19_Relacionamento_Entidades Correção/src/service/cliente.service.ts
@@ -5,15 +5,15 @@ import { ClienteRepository } from "../repository/ClienteRepository";
 export class ClienteServise{
     clienteRepository: ClienteRepository = new ClienteRepository;
 
-    async addCliente(clientedados: Cliente){
+    async addCliente(clienteDados: Cliente){
 
-        const {cpf, dataNascimento} = clientedados
+        const {cpf, dataNascimento} = clienteDados
 
         if(cpf == undefined || dataNascimento == undefined){
             throw new Error("Informações incompletas!");
         }
 
-        
+
 
     }
 }
